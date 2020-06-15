156abb68dc7d982dd7ad8e1c7c812a9e2b61550a
Rodrigo Juarez <rodrigomjuarez@gmail.com>
2020-06-15 19:54:49 -0300

    Test carouselview currentitem change when you programatically change position

---
+++ XamCarousel/XamCarousel/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace XamCarousel
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            if (BindingContext is MainViewModel vm)
+            {
+                vm.Init();
+            }
+
+            base.OnAppearing();
+        }
+    }
+}
---
+++ XamCarousel/XamCarousel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamCarousel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public MainViewModel()
+        {
+            Options = new ObservableCollection<OptionValue>();
+        }
+
+        public ICommand LabelTapCommand => new Command<int>((id) =>
+        {
+            Console.WriteLine($"Navigation to {id}");
+            Position = id;
+        });
+
+        public void Init()
+        {
+            Options.Clear();
+            Options.Add(new OptionValue(0,"a"));
+            Options.Add(new OptionValue(1,"b"));
+            Options.Add(new OptionValue(2,"c"));
+            Options.Add(new OptionValue(3,"d"));
+            Options.Add(new OptionValue(4,"e"));
+        }
+
+        private int position;
+
+        public int Position
+        {
+            get => position;
+            set
+            {
+                position = value;
+                OnPropChanged();
+            }
+        }
+
+        public ObservableCollection<OptionValue> Options { get; }
+    }
+
+    public class OptionValue
+    {
+        public OptionValue(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+}
